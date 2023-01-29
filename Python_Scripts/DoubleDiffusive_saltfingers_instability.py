224cc9c53c0ed909fae97c341e6ad68bf9c72b78
dargueso <d.argueso@uib.es>
2023-01-29 16:28:30 +0100

    Update DoubleDiffusive_saltfingers_instability.py
    Temperature difussion only

--- Python_Scripts/DoubleDiffusive_saltfingers_instability.py
+++ Python_Scripts/DoubleDiffusive_saltfingers_instability.py
@@ -29,8 +29,8 @@ logger = logging.getLogger(__name__)
 # Set problem domain
 
 # Aspect ratio 2
-Lx, Ly = (1., 2.)
-nx, ny = (256, 512)
+Lx, Ly = (1., 1.)
+nx, ny = (512, 512)
 
 # Create bases and domain
 
@@ -42,33 +42,32 @@ domain = de.Domain([x_basis,y_basis], grid_dtype=np.float64)
 nu = 1e-2
 alpha = 1e-1
 beta = 1e-2
-Kt = 1
+Kt = 1e-4
 Ks = 1e-2
 g = 9.81
 
 #Equations
 
-
-problem = de.IVP(domain, variables = ['p','u','uy','v','vy','T','Ty','S','Sy'])
+problem = de.IVP(domain, variables = ['p','u','uy','v','vy','rho','T','Ty'])
 
 problem.parameters['g'] = g
-problem.parameters['alpha'] = alpha
-problem.parameters['beta'] = beta
+problem.parameters['nu'] = nu
 problem.parameters['Kt'] = Kt
 problem.parameters['Ks'] = Ks
-problem.parameters['nu'] = nu
-problem.parameters['Tmeany'] = 100
-problem.parameters['Smeany'] = 100
+problem.parameters['alpha'] = alpha
+problem.parameters['beta'] = beta
 
-problem.add_equation("dt(u) + dx(p) - nu*(dx(dx(u)) + dy(uy)) = - u*dx(u) - v*uy")
-problem.add_equation("dt(v) + dy(p) - nu*(dx(dx(v)) + dy(vy)) = - u*dx(v) - v*vy - g*(beta*S-T*alpha)")
+
 problem.add_equation("dx(u) + vy = 0")
-problem.add_equation("dt(T) - Kt*(dx(dx(T)) + dy(Ty)) =  - v*Tmeany")
-problem.add_equation("dt(S) - Ks*(dx(dx(S)) + dy(Sy)) =  - v*Smeany")
+problem.add_equation("dt(u) + dx(p) - nu*(dx(dx(u)) + dy(uy)) = - u*dx(u) - v*uy")
+problem.add_equation("dt(v) + dy(p) - nu*(dx(dx(v)) - dy(vy))  = - g*rho - u*dx(v) - v*vy")
+problem.add_equation("rho = -alpha*T")
+problem.add_equation("dt(T) - Kt*(dx(dx(T)) + dy(Ty)) = -u*dx(T) - v*Ty")
 problem.add_equation("vy - dy(v) = 0")
 problem.add_equation("uy - dy(u) = 0")
 problem.add_equation("Ty - dy(T) = 0")
-problem.add_equation("Sy - dy(S) = 0")
+
+
 
 # Boundary conditions
 
@@ -77,10 +76,10 @@ problem.add_bc("right(u) = 0")
 problem.add_bc("left(v) = 0")
 problem.add_bc("right(v) = 0", condition="(nx != 0)")
 problem.add_bc("integ(p,'y') = 0", condition="(nx == 0)")
-problem.add_bc("left(Ty) = 0")
-problem.add_bc("right(Ty) = 0")
-problem.add_bc("left(Sy) = 0")
-problem.add_bc("right(Sy) = 0")
+problem.add_bc("left(T) = 10")
+problem.add_bc("right(T) = -10")
+
+
 
 
 #Timestepping
@@ -101,10 +100,9 @@ u = solver.state['uy']
 v = solver.state['v']
 vy = solver.state['vy']
 p = solver.state['p']
+rho = solver.state['rho']
 T = solver.state['T']
 Ty = solver.state['Ty']
-S = solver.state['S']
-Sy = solver.state['Sy']
 
 gshape = domain.dist.grid_layout.global_shape(scales=1)
 slices = domain.dist.grid_layout.slices(scales=1)
@@ -112,21 +110,24 @@ rand = np.random.RandomState(seed=42)
 noise = rand.standard_normal(gshape)[slices]
 noise1 = rand.standard_normal(gshape)[slices]
 noise2 = rand.standard_normal(gshape)[slices]
-a = 0.02
-amp = -0.2
-sigma = 0.2
-flow = -1.0
-u['g'] = 0
-#u['g'] = flow*np.tanh(y/a)
-T['g'] = 0
-S['g'] = 0
-v['g'] = noise2/400
 
+
+a = 0.02
+u['g'] = 0
+v['g'] = noise
+T['g'] = -10*np.tanh(y/a)
+rho['g'] = -alpha*T['g']
+
+xloc = 128
+Tloc = T['g'][xloc,:]
+rholoc = -alpha*Tloc
+#Sloc = S['g'][xloc,:]
+#rholoc = beta*S['g'][128,:] - alpha*T['g'][128,:]
 
 fig2, axis2 = plt.subplots(figsize=(8,5))
-line1 = axis2.plot(T['g'][128,:],np.arange(len(T['g'][128,:])))
-line2 = axis2.plot(beta*S['g'][128,:] - alpha*T['g'][128,:],np.arange(len(T['g'][128,:])),'r')
-line3 = axis2.plot(S['g'][128,:],np.arange(len(S['g'][128,:])))
+line1 = axis2.plot(Tloc,np.arange(len(Tloc)))
+line2 = axis2.plot(rholoc,np.arange(len(rholoc)),'r')
+#line3 = axis2.plot(Sloc, np.arange(len(Sloc)))
 fig2.savefig(f'./TProfile_Double-diffusive_instability_000.png')
 
 
@@ -135,7 +136,7 @@ cfl = flow_tools.CFL(solver,initial_dt,safety=0.5,threshold=0.05)
 cfl.add_velocities(('u','v'))
 
 analysis = solver.evaluator.add_file_handler('analysis_tasks', sim_dt=0.1, max_writes=50)
-analysis.add_task('T')
+# analysis.add_task('T')
 analysis.add_task('u')
 analysis.add_task('v')
 solver.evaluator.vars['Lx'] = Lx
@@ -144,29 +145,31 @@ solver.evaluator.vars['Lx'] = Lx
 x = domain.grid(0,scales=domain.dealias)
 y = domain.grid(1,scales=domain.dealias)
 xm, ym = np.meshgrid(x,y)
-fig, axis = plt.subplots(1,4,figsize=(40,20))
+
 T.set_scales(domain.dealias)
-S.set_scales(domain.dealias)
+# S.set_scales(domain.dealias)
+rho.set_scales(domain.dealias)
 u.set_scales(domain.dealias)
 v.set_scales(domain.dealias)
 vel = (u['g']**2+v['g']**2)**0.5
-rho = beta*S['g'] - alpha*T['g']
-p0 = axis[0].pcolormesh(xm, ym, S['g'].T, cmap='bwr')
-axis[0].set_title(f'Salinity (t = {solver.sim_time:2.2f})')
 
-p1 = axis[1].pcolormesh(xm, ym, T['g'].T, cmap='RdYlBu_r')
-axis[1].set_title(f'Temperature (t = {solver.sim_time:2.2f})')
 
-p2 = axis[2].pcolormesh(xm, ym, rho.T, cmap='RdBu')
-axis[2].set_title(f'Density (t = {solver.sim_time:2.2f})')
+fig, axis = plt.subplots(1,3,figsize=(40,20))
 
-p3 = axis[3].pcolormesh(xm, ym, v['g'].T, cmap='PuOr')
-axis[3].set_title(f'Vert veloc. (t = {solver.sim_time:2.2f})')
+p0 = axis[0].pcolormesh(xm, ym, rho['g'].T, cmap='RdBu')
+axis[0].set_title(f'Density (t = {solver.sim_time:2.2f})')
+
+p1 = axis[1].pcolormesh(xm, ym, v['g'].T, cmap='PuOr')
+axis[1].set_title(f'Vert veloc. (t = {solver.sim_time:2.2f})')
+
+p2 = axis[2].pcolormesh(xm, ym, T['g'].T, cmap='RdYlBu_r')
+axis[2].set_title(f'Temperature (t = {solver.sim_time:2.2f})')
 
 fig.savefig(f'./Double-diffusive_instability_000.png')
 
-
-
+# rho = beta*S['g'] - alpha*T['g']
+# p0 = axis[0].pcolormesh(xm, ym, S['g'].T, cmap='bwr')
+# axis[0].set_title(f'Salinity (t = {solver.sim_time:2.2f})')
 
 
 logger.info('Starting loop')
@@ -180,24 +183,32 @@ while solver.ok:
     if solver.iteration % 10 == 0:
         # Update plot of scalar field
         vel = (u['g']**2+v['g']**2)**0.5
-        rho = beta*S['g'] - alpha*T['g']
-        p0.set_array(np.ravel(S['g'].T)) 
-        p1.set_array(np.ravel(T['g'].T))
-        p2.set_array(np.ravel(rho.T))
-        p3.set_array(np.ravel(v['g'].T))
-        axis[0].set_title(f'Salinity (t = {solver.sim_time:2.2f})')
-        axis[1].set_title(f'Temperature  (t = {solver.sim_time:2.2f})')
-        axis[2].set_title(f'Density (t = {solver.sim_time:2.2f})')
-        axis[3].set_title(f'Vert veloc. (t = {solver.sim_time:2.2f})')
         
+        # p0.set_array(np.ravel(S['g'].T)) 
+        # p1.set_array(np.ravel(T['g'].T))
+        # axis[0].set_title(f'Salinity (t = {solver.sim_time:2.2f})')
+        # axis[1].set_title(f'Temperature  (t = {solver.sim_time:2.2f})')
+        p0.set_array(np.ravel(rho['g'].T))
+        p1.set_array(np.ravel(v['g'].T))
+        p2.set_array(np.ravel(T['g'].T))
+
+        axis[0].set_title(f'Density (t = {solver.sim_time:2.2f})')
+        axis[1].set_title(f'Vert veloc. (t = {solver.sim_time:2.2f})')
+        axis[2].set_title(f'Temperature  (t = {solver.sim_time:2.2f})')
+
         fig.canvas.draw()
         fig.savefig(f'./Double-diffusive_instability_{nt:03d}.png')
 
+        Tloc = T['g'][xloc,:]
+        rholoc = -alpha*Tloc
+        #Sloc = S['g'][xloc,:]
         fig2, axis2 = plt.subplots(figsize=(8,5))
-        line1 = axis2.plot(T['g'][128,:],np.arange(len(T['g'][128,:])))
-        line2 = axis2.plot(beta*S['g'][128,:] - alpha*T['g'][128,:],np.arange(len(T['g'][128,:])),'r')
-        line3 = axis2.plot(S['g'][128,:],np.arange(len(S['g'][128,:])))
+        line1 = axis2.plot(Tloc,np.arange(len(Tloc)))
+        line2 = axis2.plot(rholoc,np.arange(len(rholoc)),'r')
+        #line3 =  axis2.plot(Sloc, np.arange(len(Sloc)))
         fig2.savefig(f'./TProfile_Double-diffusive_instability_{nt:03d}.png')
+
+
         nt+=1
 
 
